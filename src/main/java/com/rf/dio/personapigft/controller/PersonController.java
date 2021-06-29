78deec8366d5427fa1afcb3896d0e459a2e180c1
RamonFern <ramonfernandes9@gmail.com>
2021-06-29 12:00:19 -0300

    Classes Pessoa, Phones e enum PhoneType Criado e lombok funcionando

--- src/main/java/com/rf/dio/personapigft/controller/PersonController.java
+++ src/main/java/com/rf/dio/personapigft/controller/PersonController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rf.dio.personapigft.entity.Pessoa;
+
 @RestController
 @RequestMapping("/api/v1/pessoa")
 public class PersonController {
 
 	@GetMapping
-	public String getPessoa() {
-		return "Teste API...";
+	public String getPessoa() { 
+		Pessoa p = new Pessoa();
+		p.setPrimeiroNome("Raul");
+		p.setUltimoNome("Pedro");
+		p.setCpf("32345512299");
+		return "Teste API pessoa..." + p.getUltimoNome();
 	}
 }
